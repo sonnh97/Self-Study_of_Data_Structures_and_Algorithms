ca41a6429fd7466610469deef969a293a1fe89d2
sonnh97 <hoangson.101197@gmail.com>
2023-12-11 02:38:09 +0700

    _88_Merge_Sorted_Array Two-Pointer Technique

--- LeetCode/Array/easy/_88_Merge_Sorted_Array.java
+++ LeetCode/Array/easy/_88_Merge_Sorted_Array.java
@@ -1,5 +1,28 @@
 public class _88_Merge_Sorted_Array {
 
+    public static void merge_2(int[] n1, int m, int[] n2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = (m+n) - 1;
+
+        while(k >= 0){
+            if(j < 0){
+                n1[k] = n1[i];
+                i--;
+            }else if(i < 0){
+                n1[k] = n2[j];
+                j--;
+            }else if(n1[i] > n2[j]){
+                n1[k] = n1[i];
+                i--;
+            }else{
+                n1[k] = n2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+
     public static void merge(int[] n1, int m, int[] n2, int n) {
         for (int ai : n2) {
             chenPhanTuVaoMang(ai , n1, m);
